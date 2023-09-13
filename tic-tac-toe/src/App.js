42eb2d64d56777a1374a090c2dd2b57a1a2418ec
kerat Issa <keratmy@yahoo.com>
2023-09-12 21:19:07 -0500

    commit f

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTakToe from './Components/TicTacToe/TicTakToe';
+
+function App() {
+  return (
+    <div >
+     <TicTakToe/>
+    </div>
+  );
+}
+
+export default App;
